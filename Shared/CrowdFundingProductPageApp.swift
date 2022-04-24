b1c8a62e7fcdd509dd99833c1d07c29e49d54f2e
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-24 09:50:10 +0900

    Initial Commit

---
+++ Shared/CrowdFundingProductPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrowdFundingProductPageApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CrowdFundingProductPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
